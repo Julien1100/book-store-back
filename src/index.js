8f42b1268b8fbba8b5aed0406620a5c0e65c66b0
J8T <jul.milcent@gmail.com>
2024-01-26 11:58:02 +0100

    :sparkles: Book model

--- src/index.js
+++ src/index.js
@@ -2,6 +2,14 @@ import express from "express";
 const app = express();
 const port = 1100;
 
+import mongoose from "mongoose";
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/book-store");
+}
+
 app.get("/", (req, res) => {
   res.send("Ça marche (normalement)");
 });
